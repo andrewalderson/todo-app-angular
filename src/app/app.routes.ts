3e07968cabc32ba2ecc45cd439df5d1241551744
Andrew Alderson <andrew.alderson@outlook.com>
2024-02-27 03:39:26 +0000

    chore: lazy load welcome page
    This is setup for adding authentication.
    The app will need to determine if it can log the user in automatically
    or if the welcome page needs ot be shown for the user to log in manually

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        loadComponent: async () =>
+          (await import('./nx-welcome.component')).NxWelcomeComponent,
+      },
+];
